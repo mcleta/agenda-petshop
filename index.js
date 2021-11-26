270b2e1e0355abd354c765622d1595b0ab6c386e
Murillo C Leta <murillocasemiro@gmail.com>
2021-11-26 17:26:31 -0300

    Criação da tabela Atendimentos

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 // Importando as cofigurações pro server ler
 const customExpress = require('./config/customexpress');
 const conexao = require('./infra/conexao');
+const Tabelas = require('./infra/tabelas');
 
 conexao.connect( (erro) => {
   if (erro) {
     console.log(erro);
   }
+  else{
+    console.log('DB Connection Successful');
+
+    Tabelas.init(conexao);
+
+    const app = customExpress();
+
+    // Subindo o server na porta 3000
+    app.listen(3000, () => console.log(`Server's up`));
+  }
 });
 
-const app = customExpress();
-
-// Subindo o server na porta 3000
-app.listen(3000, () => console.log(`Server's up`));
